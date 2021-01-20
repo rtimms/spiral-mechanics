cc4aa1d75f87f3337e662de277318695c532c730
Robert Timms <timms@maths.ox.ac.uk>
2021-01-20 15:49:48 +0000

    update scripts

--- plot_tension.py
+++ plot_tension.py
@@ -8,9 +8,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Parameters
-alpha = 0.1
+alphas = [0.05, 0.1, 0.15]  # expansion coefficient (list)
 delta = 0.1
-E = 1
+E = 1  # active material Young's modulus
+E_ccs = [1e3, 1e4, 1e5]  # current collector Young's modulus (list)
 nu = 1 / 3
 lam = E * nu / (1 + nu) / (1 - 2 * nu)
 mu = E / 2 / (1 + nu)
@@ -37,19 +38,6 @@ def tension(alpha):
     return np.concatenate((T1, T2))
 
 
-# Load COMSOL solution (hacky averaging for now)
-comsol1 = pd.read_csv("T1_alpha10.csv", comment="#", header=None).to_numpy()
-comsol2 = pd.read_csv("T2_alpha10.csv", comment="#", header=None).to_numpy()
-comsol3 = pd.read_csv("T3_alpha10.csv", comment="#", header=None).to_numpy()
-comsol4 = pd.read_csv("T4_alpha10.csv", comment="#", header=None).to_numpy()
-comsol5 = pd.read_csv("T5_alpha10.csv", comment="#", header=None).to_numpy()
-s_comsol = comsol3[:, 0]
-Tm_comsol = comsol3[:, 1]  # T at midpoint of current collector
-T_comsol = (
-    comsol1[:, 1] + comsol2[:, 1] + comsol3[:, 1] + comsol4[:, 1] + comsol5[:, 1]
-) / 5  # "average" over 5 equispaced curves
-
-
 # COMSOL returns T(s), so we plot everything as a function of arc length
 def arc_length(theta):
     integrand = np.sqrt(
@@ -58,15 +46,39 @@ def arc_length(theta):
     return integrate.cumtrapz(integrand, theta, initial=0)
 
 
+# Function to load COMSOL solution
+def load_comsol(E_cc, alpha):
+    path = f"data/E_cc_{int(E_cc)}/"
+    alpha100 = int(alpha * 100)
+    comsol = pd.read_csv(
+        path + f"T3_alpha{alpha100}.csv", comment="#", header=None
+    ).to_numpy()
+    s = comsol[:, 0]  # arc length (at midpoint)
+    T = comsol[:, 1]  # T at midpoint of current collector
+    return s, T
+
+
 # Plot solution(s)
-N_plot = 4
-plt.plot(arc_length(theta), tension(alpha), label="Asymptotic")
-plt.plot(s_comsol, Tm_comsol, label=f"COMSOL: T (mid)")
-plt.plot(s_comsol, T_comsol, label=f"COMSOL: T (av)")
-plt.xlim(arc_length(np.array([0, N_plot * 2 * np.pi])))
-plt.ylim([-0.20, 0.05])
-plt.xlabel("s")
-plt.ylabel("T")
-plt.title(f"T(s) in the first {N_plot} winds")
-plt.legend()
+N_plot = 5
+for E_cc in E_ccs:
+    fig, ax = plt.subplots()
+    for alpha in alphas:
+        color = next(ax._get_lines.prop_cycler)["color"]
+        s, T = load_comsol(E_cc, alpha)
+        plt.plot(
+            arc_length(theta),
+            tension(alpha),
+            "-",
+            color=color,
+            label=r"Asymptotic ($\alpha$=" + f"{alpha})",
+        )
+        plt.plot(s, T, "--", color=color, label=r"COMSOL ($\alpha$=" + f"{alpha})")
+    plt.xlim(arc_length(np.array([0, N_plot * 2 * np.pi])))
+    plt.ylim([-0.3, 0.05])
+    plt.xlabel(r"$s$")
+    plt.ylabel(r"$T(s)$")
+    plt.title(
+        f"$T(s)$ in the first {N_plot} winds \n" r"$E_{cc}/E_a$ = " + f"{int(E_cc)}"
+    )
+    plt.legend()
 plt.show()
